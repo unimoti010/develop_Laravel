9d559b942be6ab53a080ddc156f77450463cf7be
wakanaiiyama <2021php41-student021@la-study.com>
2021-05-20 15:17:49 +0900

    users以外のマイグレーショファイル追加

--- market_place/database/migrations/2021_05_19_083147_create_register_histories_table.php
+++ market_place/database/migrations/2021_05_19_083147_create_register_histories_table.php
@@ -16,10 +16,12 @@ class CreateRegisterHistoriesTable extends Migration
         Schema::create('register_histories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('user_id')->unsigned()->index;
-            $table->bigInteger('textbook_id');
+            $table->bigInteger('textbook_id')->unsigned()->index;
             $table->timestamps(); 
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('textbook_id')->references('id')->on('textbooks')->onDelete('cascade');
+
         });
     }
 
--- market_place/database/migrations/2021_05_19_083343_create_purchase_histories_table.php
+++ market_place/database/migrations/2021_05_19_083343_create_purchase_histories_table.php
@@ -15,7 +15,12 @@ class CreatePurchaseHistoriesTable extends Migration
     {
         Schema::create('purchase_histories', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
+            $table->bigInteger('user_id')->unsigned()->index;
+            $table->bigInteger('textbook_id')->unsigned()->index;
+            $table->timestamps(); 
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('textbook_id')->references('id')->on('textbooks')->onDelete('cascade');
         });
     }
 
