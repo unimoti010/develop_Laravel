b66b06345931d2bc142013b4d09436a34bb71ea8
wakanaiiyama <2021php41-student021@la-study.com>
2021-05-21 11:25:51 +0900

    HomeController書き加えたもの

--- market_place/app/Http/Controllers/Homecontroller.php
+++ market_place/app/Http/Controllers/Homecontroller.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Homecontroller extends Controller
+class HomeController extends Controller
 {
-    //
+    public function index(){
+        return view('home/index');
+    }
 }
