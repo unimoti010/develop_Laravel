4ad05e64303d8f8f6b5e25a529c82038da4b6a6e
wakanaiiyama <2021php41-student021@la-study.com>
2021-05-24 14:16:30 +0900

    教科書一覧表示

--- market_place/resources/views/commons/textbooks.blade.php
+++ market_place/resources/views/commons/textbooks.blade.php
@@ -2,25 +2,27 @@
     <thead>
         <tr>
             <th>タイトル</th>
-            <th>カテゴリ</th>
+            {{-- <th>カテゴリ</th> --}}
             <th>著者</th>
-            <th>出版社</th>
+            {{-- <th>出版社</th> --}}
             <th>価格</th>
-            <th>状態</th>
+            {{-- <th>状態</th> --}}
         </tr>
     </thead>
-    {{-- <tbody>
+    <tbody>
         @foreach($textbooks as $textbook)
             <tr>
-                <td>{{ $textbook->title}}</td>
-                {{-- <td>
+                <td>
                     <a href="{{route('textbooks.show', $textbook->id)}}">{{$textbook->title}}
                     </a>
-                </td> --}}
-                {{-- <td>{{$textbook->author}}</td>
-                <td>{{$textbook->publisher}}</td>
-            </tr> --}}
-        {{-- @endforeach --}}
-    {{-- </tbody> --}} --}}
+                </td>
+                {{-- <td>{{ $textbook->category}}</td> --}}
+                <td>{{ $textbook->author}}</td>
+                {{-- <td>{{ $textbook->publisher}}</td> --}}
+                <td>{{ $textbook->price}}</td>
+                {{-- <td>{{ $textbook->state}}</td> --}}
+            </tr>
+        @endforeach
+    </tbody>
 </table>
-{{-- {{$textbooks->links()}} --}}
+    {{-- {{ $textbooks->links() }} --}}
