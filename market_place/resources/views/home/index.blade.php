d1a55809a29f2f6e7f8e5ffa873cd841763437f6
furusawaknm <k.furusawa.php2021@gmail.com>
2021-05-25 10:28:02 +0900

    ログイン前ホームにログアウトボタン追加

--- market_place/resources/views/home/index.blade.php
+++ market_place/resources/views/home/index.blade.php
@@ -4,6 +4,7 @@
 <!--ログイン前のホーム画面-->
 <p><a href="{{ route('register') }}">新規会員登録</a></p>
 <p><a href="{{ route('login') }}">ログイン</a></p>
+<p><a href="{{ route('logout') }}">ログアウト</a></p>
 
 <!--教科書一覧を表示-->
 
