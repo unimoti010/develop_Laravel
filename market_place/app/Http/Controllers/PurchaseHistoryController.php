99d76341cc74b36c6dac9962500a845200e3f715
wakanaiiyama <2021php41-student021@la-study.com>
2021-05-25 11:28:39 +0900

    購入画面表示のためのnotificationメソッドOK

---
+++ market_place/app/Http/Controllers/PurchaseHistoryController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Textbook;
+
+class PurchaseHistoryController extends Controller
+{
+    public function index()
+    {
+        $textbooks = \Auth::user()->purchase_histories()
+        ->orderby('created_at', 'desc')->paginate(20);
+        return view('purchase_histories/index', ['textbooks' => $textbooks]);
+    }
+    public function notification(Request $request) //
+    {
+        $user_id = \Auth::id();//ログインユーザーのID
+
+        $textbook = Textbook::find($request->id);
+
+        return view('purchase.notification', ['textbooks' => [$textbook]]);
+    }
+    public function store()
+    {
+        
+    }
+
+}
