85f74fc96db5a9adaf150af2ae7208e63aed4c78
wakanaiiyama <2021php41-student021@la-study.com>
2021-05-26 18:25:41 +0900

    ログイン後ホーム、かぶっていたところを消した

--- market_place/resources/views/user/index.blade.php
+++ market_place/resources/views/user/index.blade.php
@@ -11,8 +11,6 @@
 <!--あとでリンク先を追加する-->
 <p><a href="{{ route('purchase_histories.index') }}">購入履歴</a></p>
 
-{{-- <p><a href="{{route('textbooks.index')}}">教科書一覧</a></p> --}}
-
 {{--p.126参照--}}
 <form id="logout-form" action="{{ route('logout') }}" method="POST">
     @csrf
