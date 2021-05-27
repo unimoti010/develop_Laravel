3329fa0cf3659da396b72cb0381fb1dce9320d9f
wakanaiiyama <2021php41-student021@la-study.com>
2021-05-27 11:55:17 +0900

    コミットアウトを消して整理

--- market_place/app/Http/Controllers/PurchaseHistoryController.php
+++ market_place/app/Http/Controllers/PurchaseHistoryController.php
@@ -14,9 +14,6 @@ class PurchaseHistoryController extends Controller
 {
     public function index()
     {
-        
-        // $textbooks = Textbook::whereHas('purchase_history', function($query){
-        // $query->where('user_id', '=', \Auth::id());})->paginate(20);
         $textbooks = DB::select('select title, price, purchase_histories.created_at from textbooks 
         join purchase_histories on textbooks.id = purchase_histories.textbook_id 
         where purchase_histories.user_id = ' .\Auth::id(). ' order by purchase_histories.created_at desc ');
@@ -28,60 +25,18 @@ class PurchaseHistoryController extends Controller
 
         $stmt->execute();
         $saved_info = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        // ddd($saved_info);
-
-        //Textbookモデルから、purchase_historiesテーブルのuser_idとログインユーザが一致しているもの
-        //つまりログインユーザの購入した本の履歴
-
-        // $textbooks = \App\Textbook::select()
-        // ->join('purchase_history', 'user_id', '=', \Auth::id())
-        // ->where('');
-
-        // ddd($textbooks);
-        // $stdTextbooks = [];
-        // foreach($textbooks as $textbook){
-        //     $stdTextbook = new \stdClass();
-        //     $stdTextbook -> title = $textbook -> title;
-        //     $stdTextbook -> price = $textbook -> price;
-        //     $stdTextbook -> created_at = $textbook -> created_at;
-        //     array_push($stdTextbooks, $stdTextbook);
-        // };
-        // ddd(gettype($stdTextbooks));
+        
         return view('purchase/index', ['textbooks' => $saved_info]);
 
-
-        // $textbooks = \Auth::user()->purchase_histories()
-        // ->orderby('created_at', 'desc')->paginate(20);
-
-        // $textbooks = PurchaseHistory::with('textbook')->get();
-
-        // $textbooks = Textbook::with([
-        //     'purchase_history'=> function (Builder $query){
-        //         $query->where('user_id', '=', \Auth::id());}])->paginate(20);
-
-
-        // $textbooks = PurchaseHistory::all()->textbook()->where('user_id', '=', \Auth::id() )->paginate(20);
     }
 
 
-    public function notification(Request $request) //
+    public function notification(Request $request) 
     {
-        $user_id = \Auth::id();//ログインユーザーのID
+        $user_id = \Auth::id();
 
         $textbook = Textbook::find($request->id);
 
         return view('purchase/notification', ['textbooks' => [$textbook]]);
     }
-
-
-    // public function store(Request $request)
-    // {
-    //     $textbook = new Textbook;
-    //     $textbook->id = $request->id;
-    //     $textbook->save();
-
-    //     $user_id = \Auth::id();
-    //     $user_id->save();
-    // }
-
 }
