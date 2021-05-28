0bc552062bc30b3a2470688b6713926aa3140320
furusawaknm <k.furusawa.php2021@gmail.com>
2021-05-28 14:44:30 +0900

    no message

--- market_place/resources/views/home/index.blade.php
+++ market_place/resources/views/home/index.blade.php
@@ -14,7 +14,21 @@
 <p><a href="{{ route('login') }}">ログイン</a></p>
 @endguest
 
+{{-- ログイン後 --}}
 @auth
+<style>
+    body {
+        font-size: 120%;
+        background-image: url("images/textbook2.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position-y:120px ;
+    }
+    p{
+        padding-top: 20px:
+        line-height: 160%;
+    }
+</style>
 <p><a href="{{ route('users.edit', $user) }}">会員情報変更</a></p>
 @cannot('isAdmin')
 <p><a href="{{ route('textbooks.index') }}">教科書一覧</a></p>
