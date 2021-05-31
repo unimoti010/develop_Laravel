9bb3fad1d3c1550cdf07d75390d0733df36523d9
unimoti010 <2021php41-student010@la-study.com>
2021-05-31 11:04:49 +0900

    整理のみ

--- market_place/resources/views/home/index.blade.php
+++ market_place/resources/views/home/index.blade.php
@@ -2,21 +2,8 @@
 
 @section('content')
 
-{{-- @section('content') --}}
-
-{{-- 管理者のみ --}}
-{{-- @can('isAdmin')
-<p><a href="{{ route('admin.allUsers') }}" class="btn btn-outline-primary">会員一覧</a></p>
-<p><a href="{{ route('admin.allTextbooks') }}" class="btn btn-outline-primary">教科書一覧</a></p>
-@endcan --}}
-
 {{-- ログイン前のみ --}}
 @guest
-
-{{-- <ul class="home">
-    <li><a href="{{ route('register') }}" class="btn btn-outline-primary">新規会員登録</a></li>
-    <li><a href="{{ route('login') }}" class="btn btn-outline-primary">ログイン</a></li>
-</ul> --}}
 <video class="video1home" autoplay muted playsinline src="/images/textbook3-1.mp4" loop></video>
 <div class="beforelogin">
     <p>商品のご登録・ご購入には、<a href="{{ route('register') }}">会員登録</a>が必要です</p>
@@ -26,24 +13,12 @@
 
 {{-- ログイン後 --}}
 @auth
-{{-- <p><a href="{{ route('users.edit', $user) }}" class="btn btn-outline-primary">会員情報変更</a></p>
-@cannot('isAdmin')
-<p><a href="{{ route('textbooks.index') }}" class="btn btn-outline-primary">教科書一覧</a></p>
-<p><a href="{{ route('textbooks.create') }}" class="btn btn-outline-primary">教科書登録</a></p>
-<p><a href="{{ route('register_histories.index') }}" class="btn btn-outline-primary">登録履歴</a></p>
-<p><a href="{{ route('purchase_histories.index') }}" class="btn btn-outline-primary">購入履歴</a></p>
-@endcannot --}}
-
 
 <div class="video2home">
     <img src="/images/smartphone.jpg" width="350px" height="197px">
     <video autoplay muted playsinline src="/images/textbookhome1.mp4" loop width="350px"></video>
     <img src="/images/library1.jpg" width="350px" height="197px">
 </div>
-    {{--<form action="{{ route('logout') }}" method="post">
-    @csrf
-    <input type="submit" class="btn btn-warning" value="ログアウト"> --}}
-
 
 @endauth
 
--- market_place/resources/views/layouts/app.blade.php
+++ market_place/resources/views/layouts/app.blade.php
@@ -12,50 +12,14 @@
     <link rel="stylesheet" href="/css/test.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css"
-        href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/reset.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/vegas/2.4.4/vegas.min.css">
-
 </head>
 <style>
-    body {
-        background-color: #f5f5f5;
-    }
-
-    .app-main {
-        background-image: url('/images/header-bg.jpg');
-        background-position: center center;
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-attachment: fixed;
-    }
-
-    .logo {
-        margin: 0.2em auto;
-        display: block;
-    }
-
-    a {
-        text-decoration: none;
-    }
+    
 </style>
 
 <body>
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
-    </script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
-    </script>
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"
-        integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous">
-    </script> --}}
-
+    
     <header>
 
         <nav class="navbar navbar-expand-lg bg-white border border-2 p-1">
@@ -124,6 +88,19 @@
             @yield('content')
         </div>
     </main>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+
 </body>
 
 </html>
